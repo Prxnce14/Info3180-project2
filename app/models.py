515ff807e9456dbbe7c1ef31b94b7506e393cdba
Nicholas Joiles <kemanijoiles@gmail.com>
2024-04-12 04:09:38 -0500

    addded the users table

--- app/models.py
+++ app/models.py
@@ -29,4 +29,40 @@ class insiders(db.Model):
 
 
 
-#model for 
+#model for likes
+
+
+#model for Follows
+
+
+
+#model for Users
+
+class insiders2(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    location = db.Column(db.String(255))
+    biography = db.Column(db.String(255))
+    profile_photo = db.Column(db.String(255))
+    created_on = db.Column(db.DateTime())
+
+
+    def __init__(self, uname, pword, fname, lname, em, local, bio, pphoto, create):
+        self.username = uname
+        self.password = pword
+        self.firstname = fname
+        self.lastname = lname
+        self.email = em
+        self.location = local
+        self.biography = bio
+        self.profile_photo = pphoto
+        self.created_on = create
+        
+    def __repr__(self):
+        return '<insiders2 %r>' % self.title
